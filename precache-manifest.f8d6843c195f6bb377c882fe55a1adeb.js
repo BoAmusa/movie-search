ea04f88590d3b679cedfde997c7a5fbc6534b71a
Bo Amusa <Boamusa@gmail.com>
2022-08-11 22:31:27 -0400

    Updates

---
+++ precache-manifest.f8d6843c195f6bb377c882fe55a1adeb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/movie-search/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "09bba5c020a13ab6cc77",
+    "url": "/movie-search/static/js/runtime~main.09bba5c0.js"
+  },
+  {
+    "revision": "fc81361503df856750dc",
+    "url": "/movie-search/static/js/main.fc813615.chunk.js"
+  },
+  {
+    "revision": "735123c506061fba3b3d",
+    "url": "/movie-search/static/js/2.735123c5.chunk.js"
+  },
+  {
+    "revision": "be54c62ab17bc2e698b5",
+    "url": "/movie-search/static/js/1.be54c62a.chunk.js"
+  },
+  {
+    "revision": "fc81361503df856750dc",
+    "url": "/movie-search/static/css/main.70dac56f.chunk.css"
+  },
+  {
+    "revision": "735123c506061fba3b3d",
+    "url": "/movie-search/static/css/2.c7762b16.chunk.css"
+  },
+  {
+    "revision": "e8e5c8080835d90adf11b0a611ce9403",
+    "url": "/movie-search/index.html"
+  }
+];
