a4ab70b219e3573b91f519f2fb3eabadf60bd892
Bo Amusa <Boamusa@gmail.com>
2022-08-11 23:41:32 -0400

    Temp changes

--- src/Components/Movie.js
+++ src/Components/Movie.js
@@ -1,8 +1,32 @@
 import react from "react";
 
-const Movie = ({ title, image }) => (
+const API_KEY = process.env.REACT_APP_IMDB_API_KEY;
+const TRAILER_API = `https://imdb-api.com/API/Trailer/${API_KEY}/`;
+
+//API to retrieve movie trailer via IMdB
+const getMovieTrailer = async (API) => {
+  const response = await fetch(API);
+  const responseJson = await response.json();
+  const trailer = responseJson.results;
+
+  if (trailer !== undefined && trailer !== null && trailer.length > 0) {
+    window.open(trailer.link, trailer.title);
+  }
+};
+
+//Executes trailer redirection on click
+const handleOnPosterClick = (value) => {
+  getMovieTrailer(TRAILER_API + value);
+};
+
+const Movie = ({ title, image, id }) => (
   <div className="grow dib">
-    <img class="card__image" src={image} alt={title} />
+    <img
+      class="card__image"
+      src={image}
+      alt={title}
+      onClick={handleOnPosterClick(id)}
+    />
     <div className="card__content">
       <h3>{title}</h3>
     </div>
