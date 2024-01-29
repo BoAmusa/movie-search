3c02c601a396fdcc2a1183698cf71d15a52d2c44
Bo Amusa <Boamusa@gmail.com>
2024-01-29 08:13:40 -0500

    Add search icon

--- src/Components/SearchBox.js
+++ src/Components/SearchBox.js
@@ -1,15 +1,26 @@
 import React from "react";
+import { BsSearch } from "react-icons/bs";
+
+
 
 const SearchBox = (props) => {
   return (
-    <div className="mv4 w-100 w-50-m w-25-l mw6 center">
+   <div className="mv4 w-90 w-45-m w-20-l mw6 center relative">
       <form onSubmit={event => props.onSubmit(event)}>
-      <input
-        className="pa3 ba b--green bg-lightst-blue center"
-        type="search"
-        placeholder="search movies"
-        onChange={event => props.onChange(event.target.value)}
-      />
+        <div className="flex items-center">
+          <input
+            className="pa3 ba b--green bg-lightst-blue center flex-grow-1"
+            type="search"
+            placeholder="search movies"
+            onChange={event => props.onChange(event.target.value)}
+          />
+          <button
+            type="submit"
+            className="pa3 bg-lightst-blue ba b--green pointer absolute right-0"
+          >
+            <BsSearch className="fas fa-search"></BsSearch>
+          </button>
+        </div>
       </form>
     </div>
   );
